fdcf8e396cbcc4a8b995d0244c9718fd614c6222
chouisme <C109156112@nkust.edu.tw>
2021-04-30 22:29:21 +0800

    1

--- 第一次midterm/練習19.py
+++
@@ -1,5 +0,0 @@
-student = {"123":"TOM DTGD","456":"CAT CSIE","789":"NANA ASIE","321":"LIM DBA","654":"WON FDD"}
-ser = input("輸入查詢學號為：")
-print(type(ser))
-print(student[ser])
-
--- 第一次midterm/練習9.py
+++
@@ -1,6 +0,0 @@
-s1 = list(input("輸入一句句子"))
-s2 = list(input("輸入一句句子"))
-if s1<=s2:
-    print("YES")
-else:
-    print("NO")
